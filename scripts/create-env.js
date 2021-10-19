ecf419be3a54d466225c958fdd5c96381ffda93f
CarlosAyala <carlosylautaro@gmail.com>
2021-10-19 04:21:08 -0300

    Test 1.1

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,8 +1,8 @@
-/* const fs = require('fs');
+const fs = require('fs');
 
 fs.writeFileSync(
   './.env',
-  `LOCATION_API_KEY=${process.env.LOCATION_API_KEY}\n `,
-  `USER_API_WEATHER=${process.env.USER_API_WEATHER}\n `,
-  `PASSWORD_API_WEATHER=${process.env.PASSWORD_API_WEATHER}\n `,
-); */
+  `LOCATION_API_KEY=${process.env.LOCATION_API_KEY}\n 
+  USER_API_WEATHER=${process.env.USER_API_WEATHER}\n 
+  PASSWORD_API_WEATHER=${process.env.PASSWORD_API_WEATHER}\n`,
+);
